696890fd8fe055b5e36dbd42e584a00563afcab7
Dave Coleman <dave@dvclmn.com>
2026-04-02 12:39:10 +0200

    Trying to handle case where Env value (for syncing) may be optional

--- Sources/InteractionPrimitives/EnvironmentValueSync.swift
+++ Sources/InteractionPrimitives/EnvironmentValueSync.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 private struct EnvironmentSyncModifier<Value, ID: Equatable>: ViewModifier {
 
-  @Environment private var value: Value
-  let id: (Value) -> ID
-  let apply: (Value) -> Void
+  @Environment private var value: Value?
+  let id: (Value?) -> ID
+  let apply: (Value?) -> Void
 
   init(
-    _ keyPath: KeyPath<EnvironmentValues, Value>,
-    id: @escaping (Value) -> ID,
-    apply: @escaping (Value) -> Void,
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    id: @escaping (Value?) -> ID,
+    apply: @escaping (Value?) -> Void,
   ) {
     _value = Environment(keyPath)
     self.id = id
@@ -37,18 +37,26 @@ extension View {
   ///   .syncModifiers(to: $modifiers)
   /// ```
   public func syncEnvironment<Value: Equatable>(
-    _ keyPath: KeyPath<EnvironmentValues, Value>,
-    to binding: Binding<Value>,
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    to binding: Binding<Value?>,
   ) -> some View {
     syncEnvironment(keyPath) { binding.wrappedValue = $0 }
   }
 
   public func syncEnvironment<Value, ID: Equatable>(
-    _ keyPath: KeyPath<EnvironmentValues, Value>,
-    using idKeyPath: KeyPath<Value, ID>,
-    to binding: Binding<Value>,
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    using idKeyPath: KeyPath<Value?, ID>,
+    to binding: Binding<Value?>,
   ) -> some View {
-    syncEnvironment(keyPath, using: idKeyPath) { binding.wrappedValue = $0 }
+    syncEnvironment(keyPath, id: { $0[keyPath: idKeyPath] }, to: binding)
+  }
+
+  public func syncEnvironment<Value, ID: Equatable>(
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    id: @escaping (Value?) -> ID,
+    to binding: Binding<Value?>,
+  ) -> some View {
+    syncEnvironment(keyPath, id: id) { binding.wrappedValue = $0 }
   }
 
   /// ## Custom closure
@@ -60,23 +68,34 @@ extension View {
   ///   }
   /// ```
   public func syncEnvironment<Value: Equatable>(
-    _ keyPath: KeyPath<EnvironmentValues, Value>,
-    apply: @escaping (Value) -> Void,
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    apply: @escaping (Value?) -> Void,
   ) -> some View {
     modifier(EnvironmentSyncModifier(keyPath, id: { $0 }, apply: apply))
   }
 
   public func syncEnvironment<Value, ID: Equatable>(
-    _ keyPath: KeyPath<EnvironmentValues, Value>,
-    using idKeyPath: KeyPath<Value, ID>,
-    apply: @escaping (Value) -> Void,
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    using idKeyPath: KeyPath<Value?, ID>,
+    apply: @escaping (Value?) -> Void,
   ) -> some View {
-    modifier(
-      EnvironmentSyncModifier(
-        keyPath,
-        id: { $0[keyPath: idKeyPath] },
-        apply: apply,
-      )
-    )
+    syncEnvironment(keyPath, id: { $0[keyPath: idKeyPath] }, apply: apply)
   }
+
+  //  public func syncEnvironment<Value, ID: Equatable>(
+  //    _ keyPath: KeyPath<EnvironmentValues, Value>,
+  //    using idKeyPath: KeyPath<Value, ID>,
+  //    apply: @escaping (Value) -> Void,
+  //  ) -> some View {
+  //    syncEnvironment(keyPath, id: { $0[keyPath: keyPath]?[keyPath: idKeyPath] }, apply: apply)
+  //  }
+
+  public func syncEnvironment<Value, ID: Equatable>(
+    _ keyPath: KeyPath<EnvironmentValues, Value?>,
+    id: @escaping (Value?) -> ID,
+    apply: @escaping (Value?) -> Void,
+  ) -> some View {
+    modifier(EnvironmentSyncModifier(keyPath, id: id, apply: apply))
+  }
+
 }
