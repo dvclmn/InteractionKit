b3295d0b30092da5fa2deade52448bbbdb167f7c
Dave Coleman <dave@dvclmn.com>
2026-04-15 11:06:07 +0200

    Changes overall package name to Lilypad. General 'interaction' code...
    ... has moved to BasePrimitives

--- Package.swift
+++ Package.swift
@@ -3,15 +3,14 @@
 import PackageDescription
 
 let package = Package(
-  name: "InteractionKit",
+  name: "Lilypad",
   platforms: [
     .macOS("14.0")
   ],
   products: [
     .library(
-      name: "InteractionKit",
+      name: "Lilypad",
       targets: [
-//        "InteractionKit",
         "Lilypad",
         "PaintKit",
       ],
@@ -23,47 +22,14 @@ let package = Package(
   ],
 
   targets: [
-//    .target(
-//      name: "InteractionKit",
-//      dependencies: [
-//        "BasePrimitives"
-//        //        .product(name: "BasePrimitives", package: "BasePrimitives")
-//      ],
-//    ),
     .target(
       name: "PaintKit",
-      dependencies: [
-        "Lilypad"
-//        .module(.lilypad)
-      ],
+      dependencies: ["Lilypad"],
     ),
 
     .target(
       name: "Lilypad",
       dependencies: ["BasePrimitives"],
     ),
-    //    .testTarget(
-    //      name: "GestureKitTests",
-    //      dependencies: ["GestureKit"],
-    //    ),
   ],
 )
-
-extension Target.Dependency {
-  static func module(_ name: InternalModule) -> Self {
-    .target(name: name.name)
-  }
-}
-extension String { static let baseHelpers = "BaseHelpers" }
-
-enum InternalModule {
-//  case interactionKit
-  case lilypad
-
-  var name: String {
-    switch self {
-//      case .interactionKit: ("InteractionKit")
-      case .lilypad: ("Lilypad")
-    }
-  }
-}
